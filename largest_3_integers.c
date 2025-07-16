028e208d2514196751b09d1edf786fea851229b1
Mandar Kale <mandarkale12@gmail.com>
2025-07-16 16:38:19 +0530

    Add files via upload
    Program to find largest among 3 integers

---
+++ largest_3_integers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c, largest;
+    scanf("%d %d %d",&a, &b, &c);
+    if(a >= b){
+        if(a >= c){
+            largest = a;
+        }else{
+            largest = c;
+        }
+    }else{
+        if(b >= c){
+            largest = b;
+        }else{
+            largest = c;
+        }
+    }
+    printf("%d", largest);
+    
+    return 0;
+}
